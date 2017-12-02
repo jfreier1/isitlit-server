def9864e9fa42a1a9ee703008133aa4d3f6a616b
Joshua Freier <jsfreier@gmail.com>
2017-12-02 16:26:19 -0500

    yay, server up

--- project.clj
+++ project.clj
@@ -5,10 +5,15 @@
                   [korma "0.4.3"]
                   [org.clojure/java.jdbc "0.6.1"]
                   [mysql/mysql-connector-java "5.1.25"]
+                  [ring "1.6.3"]
+                  [puppetlabs/ring-middleware "1.0.0"]
+                  [ring/ring-json "0.4.0"]
+                  [javax.servlet/servlet-api "2.5"]
                   [log4j "1.2.15" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]]
+   :plugins [[lein-ring "0.12.1"]]
    :ring {:handler isitlit-server.handler/app}
    :uberjar-name "server.jar"
    :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
